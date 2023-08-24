5569f5ae6a5791517fed0a55489210521072897a
Danial Motevali <danial.motevali.82@gmail.com>
2023-08-24 16:30:40 +0330

    No mag

---
+++ MVC/DAL/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC.Models;
+
+namespace MVC.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<City> Cities{ get; set; }
+        public DbSet<Personnel> Personnel { get; set; }
+        public DbSet<Representation> Representations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>(x =>
+            {
+                x.HasKey(x => x.CityId);
+                x.HasMany(y => y.Representations).WithOne(x => x.City).HasForeignKey(x => x.RepresentationId);
+            });
+
+            modelBuilder.Entity<Personnel>(x =>
+            {
+                x.HasOne(x => x.Representation).WithMany(x => x.Personnels).HasForeignKey(x => x.RepresentationId);
+            });
+        }
+    }
+}
